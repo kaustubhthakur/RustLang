02b0e515cef18f691d270a57afa6bed5b881f6db
Kaustubh <kaustubhthakur76@gmail.com>
2023-02-19 00:53:10 +0530

    secondary commit

---
+++ FirstProgram/formatted.rs
@@ -0,0 +1,8 @@
+  // In general, the `{}` will be automatically replaced with any
+    // arguments. These will be stringified.
+fn main()
+{
+
+    println!("day {} {subject}",69,subject=" telling my mom i can code");
+
+}
